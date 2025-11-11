786d5c989dc2f44c0b1bc8f367ce77d3a070bc21
Anton Tseysnovskiy <antontseysnovskiy@gmail.com>
2025-11-12 00:31:16 +0300

    - Registration page completed 1.0

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'login_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -15,166 +16,197 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              // Заголовок
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.only(left: 30.0, top: 80.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Create\nAccount',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 55,
-                      fontFamily: 'Raleway',
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 130),
-
-              // Основная форма
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(horizontal: 30.0),
-                child: Column(
-                  children: [
-                    // Email
-                    Container(
-                      height: 52,
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 248, 248, 248),
-                        borderRadius: BorderRadius.circular(100),
-                      ),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Email',
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 15,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 15),
-
-                    // Password
-                    Container(
-                      height: 52,
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 248, 248, 248),
-                        borderRadius: BorderRadius.circular(100),
-                      ),
-                      child: TextField(
-                        obscureText: !_isPasswordVisible,
-                        decoration: InputDecoration(
-                          hintText: 'Password',
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 15,
-                          ),
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                              _isPasswordVisible
-                                  ? Icons.visibility
-                                  : Icons.visibility_off,
-                              color: Colors.grey,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _isPasswordVisible = !_isPasswordVisible;
-                              });
-                            },
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 15),
-
-                    // Phone number
-                    Container(
-                      height: 52,
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 248, 248, 248),
-                        borderRadius: BorderRadius.circular(100),
-                      ),
-                      child: IntlPhoneField(
-                        decoration: InputDecoration(
-                          hintText: 'Your number',
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.only(
-                            left: 20,
-                            right: 20,
-                            top: 50,
-                          ),
-                          floatingLabelBehavior:
-                              FloatingLabelBehavior.never, // важная строка
-                        ),
-                        initialCountryCode: 'RU',
-                        showDropdownIcon: false, // убираем стрелку
-                        flagsButtonPadding: EdgeInsets.only(left: 20, top: 14),
-                        keyboardType: TextInputType.number,
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly,
-                        ],
-                        textAlignVertical: TextAlignVertical
-                            .center, // центрирование по вертикали
-                        onChanged: (phone) {
-                          print(phone.completeNumber);
-                        },
-                        style: TextStyle(fontSize: 14),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              // Кнопка Done
-              Padding(
-                padding: EdgeInsets.only(
-                  top: 60.0,
-                  left: 30.0,
-                  right: 30.0,
-                  bottom: 30,
-                ),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 61,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(255, 0, 76, 255),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()),
-                      );
-                    },
-                    child: Text(
-                      'Done',
-                      style: TextStyle(
-                        fontSize: 22,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w300,
-                        fontFamily: 'Nunito Sans',
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+      body: Stack(
+        children: [
+          // ------------------ SVG на заднем фоне ------------------
+          Positioned(
+            top: 50,
+            left: 290,
+            child: SvgPicture.asset(
+              'assets/images/registration_page/bubble 01.svg',
+              width: 250,
+              height: 250,
+            ),
           ),
-        ),
+          Positioned(
+            bottom: 540,
+            right: 165,
+            child: SvgPicture.asset(
+              'assets/images/registration_page/bubble 02.svg',
+              width: 250,
+              height: 250,
+              // colorFilter: ColorFilter.mode(
+              //   Color.fromARGB(49, 0, 0, 0),
+              //   BlendMode.srcIn,
+              // ),
+            ),
+          ),
+
+          // ------------------ Основной контент ------------------
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // Заголовок
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.only(left: 30.0, top: 80.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Create\nAccount',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 55,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 130),
+
+                  // Основная форма
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.symmetric(horizontal: 30.0),
+                    child: Column(
+                      children: [
+                        // Email
+                        Container(
+                          height: 52,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 248, 248, 248),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Email',
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+
+                        // Password
+                        Container(
+                          height: 52,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 248, 248, 248),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: TextField(
+                            obscureText: !_isPasswordVisible,
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 15,
+                              ),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _isPasswordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                  color: Colors.grey,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _isPasswordVisible = !_isPasswordVisible;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+
+                        // Phone number
+                        Container(
+                          height: 52,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 248, 248, 248),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: IntlPhoneField(
+                            decoration: InputDecoration(
+                              hintText: 'Your number',
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.only(
+                                left: 20,
+                                right: 20,
+                                top: 50,
+                                bottom: 0,
+                              ),
+                              floatingLabelBehavior:
+                                  FloatingLabelBehavior.never,
+                            ),
+                            initialCountryCode: 'RU',
+                            showDropdownIcon: false,
+                            flagsButtonPadding: EdgeInsets.only(left: 20),
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly,
+                            ],
+                            textAlignVertical: TextAlignVertical.center,
+                            onChanged: (phone) {
+                              print(phone.completeNumber);
+                            },
+                            style: TextStyle(fontSize: 14),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // Кнопка Done
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 60.0,
+                      left: 30.0,
+                      right: 30.0,
+                      bottom: 30,
+                    ),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 61,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 0, 76, 255),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginScreen(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          'Done',
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            fontFamily: 'Nunito Sans',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
